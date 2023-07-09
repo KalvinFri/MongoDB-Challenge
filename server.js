deab6253ae2f7ee93938a2ca76b083127de0fba2
TXT-47 <txt-47@TXT-47.lan>
2023-07-08 18:10:51 -0700

    update

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const BlogModel = require('./models/blogs');
+const {MongoClient} = require('mongodb')
+const ProfileModel = require('./models/profile');
 
-const app = express()
-const port = 8080;
+const app = express();
+const port = process.env.PORT;
+const uri = process.env.MONGO_DB_URI;
+const client = new MongoClient(uri);
 
 // set up middleware
 app.use(cors({origin: true, credentials: true}));
@@ -20,21 +23,57 @@ mongoose.connect(process.env.MONGO_DB_URI)
 // routes
 app.get('/', (req, res) => res.status(200).send('Server is Running.'));
 
-app.post('/add-blog', (req, res) => {
-    const incomingData = req.body;
+    app.get('/homepage', (req, res) => {
+        async function run() {
+        try {
+            const database = client.db("blog-forms");
+            const profiles = database.collection("profiles");
+            const result = await profiles.find().toArray();
+            res.status(200).send(result);
+        } catch (err) {
+            console.log(err);
+        };
+        }
+        run().catch(console.dir);
+    });    
 
+app.post('/homepage', (req, res) => {
+        const incomingData = req.body;
+    
+        try {
+            const newProfile = new ProfileModel(incomingData);
+            newProfile.save();
+    
+            res.status(200).send({
+                message: 'Saved Profile'
+            })
+        } catch (err) {
+            console.log(err);
+        }
+    });
+
+app.delete('/homepage', (req, res) => {
+async function run() {
     try {
-        const newBlog = new BlogModel(incomingData);
-        newBlog.save();
-
+      const database = client.db("blog-forms");
+      const profiles = database.collection("profiles");
+      const result = await profiles.deleteOne();
+      if (result.deletedCount === 1) {
         res.status(200).send({
-            message: 'saved blog'
-        })
+            message: 'Successfully deleted one document.'
+        });
+      } else {
+        res.status(200).send({
+            message: 'No documents found.'
+        });
+      }
     } catch (err) {
-        console.log(err);
+        console.log(err)
     }
+  }
+  run().catch(console.dir);
 });
 
 app.listen(port, () => {
-    console.log(`⚡Server is running 👉 https://localhost:${port}`);
+    console.log(`Server is running at https://localhost:${port}`);
 });
